15b744ce4856e7e86395cc4766cf49a537c50aa8
ELEVEN <lov.2014@gmail.com>
2016-08-02 17:56:05 +0800

    chore: config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,10 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    semi: [2, 'never'],
     'import/no-unresolved': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': process.env.NODE_ENV === 'production' ? ['error'] : ['error', {'allow': ['warn', 'error','log']}],
   }
 }
