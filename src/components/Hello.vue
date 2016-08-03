fd34850681b84e8df42e3c57aa938e4d78ded424
ELEVEN <lov.2014@gmail.com>
2016-08-03 14:59:23 +0800

    dev: previewer

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,24 +1,63 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <img v-for="(index, item) in list" :src="item.src" @click="$refs.previewer.show(index)" width="100" alt="" class="previewer-demo-img" />
+
+    <previewer :list="list" :options="options" v-ref:previewer></previewer>
+
+    <p>
+      PhotoSwipe
+      this.photoswipe && this.photoswipe.destroy() 报错
+      Uncaught TypeError: Cannot read property 'destroy' of null
+    </p>
   </div>
 </template>
 
 <script>
+import { Previewer } from 'vux-components'
+
 export default {
+  name: 'Hello',
+
+  components: {
+    Previewer,
+  },
+
   data() {
     return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
       msg: 'Hello World!',
+      list: [
+        {
+          src: 'https://placekitten.com/600/400',
+          w: 600,
+          h: 400,
+        },
+        {
+          src: 'https://placekitten.com/1200/900',
+          w: 1200,
+          h: 900,
+        },
+      ],
+      options: {
+        getThumbBoundsFn(index) {
+          // find thumbnail element
+          const thumbnail = document.querySelectorAll('.previewer-demo-img')[index]
+          // get window scroll Y
+          const pageYScroll = window.pageYOffset || document.documentElement.scrollTop
+          // optionally get horizontal scroll
+          // get position of element relative to viewport
+          const rect = thumbnail.getBoundingClientRect()
+          // w = width
+          return { x: rect.left, y: rect.top + pageYScroll, w: rect.width }
+          // Good guide on how to get element coordinates:
+          // http://javascript.info/tutorial/coordinates
+        },
+      },
     }
   },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1 {
   color: #42b983;
